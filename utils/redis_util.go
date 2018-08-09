067d36eaeab27a954a6ac5bc08b6c1eb20d39776
guofacheng <guofacheng@chainup.com>
2018-08-09 18:11:20 +0800

    del log

--- utils/redis_util.go
+++ utils/redis_util.go
@@ -3,7 +3,6 @@ package utils
 import (
 	"github.com/gomodule/redigo/redis"
 	"time"
-	"log"
 )
 
 var RedisPool *redis.Pool
@@ -16,13 +15,11 @@ func init()  {
 		Dial: func() (redis.Conn, error) {
 			con,err1 :=redis.Dial("tcp","45.77.82.85:6388")
 			if err1 != nil {
-				log.Fatal(err1)
-				con.Close()
+				panic(err1)
 			}
 			_,err2 := con.Do("AUTH","1q2w3e4r")
 			if err2 != nil {
-				log.Fatal(err2)
-				con.Close()
+				panic(err2)
 			}
 			return con,err1
 		},
