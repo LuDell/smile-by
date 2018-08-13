3a003252e39564c3bb4882ae114ef3ac44f218ff
guofacheng <guofacheng@chainup.com>
2018-08-13 16:01:23 +0800

    add code

--- app_test.go
+++ app_test.go
@@ -3,18 +3,23 @@ package main
 import (
 	"fmt"
 	"testing"
-	"time"
+	"strconv"
 )
 
 func Test_run(t *testing.T)  {
-	var i = 0
-	for  i = 0 ;i<10;i++ {
-		go func (i int)  {
-			i = i + 100
-			fmt.Println(i)
+	var chan1 = make(chan string,3)
+	for i :=0; i<10; i++ {
+		go func(num int) {
+			var temp= "chan" + strconv.FormatInt(int64(num),10)
+			chan1 <- temp
 		}(i)
 	}
-	time.Sleep(1*time.Second)
+	for  i := 0 ; i<10; i++ {
+		go func(chanTemp chan string) {
+			var temp = <-chan1
+			fmt.Println("读取",temp)
+		}(chan1)
+	}
 }
 
 
