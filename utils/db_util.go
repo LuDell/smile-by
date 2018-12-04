b67971a34ea22666cf1df255abaf3c96f2ed0477
guofacheng <guofacheng@chainup.com>
2018-12-04 20:58:40 +0800

    add db config

--- utils/db_util.go
+++ utils/db_util.go
@@ -4,6 +4,8 @@ import (
 	_"github.com/go-sql-driver/mysql"
 	"database/sql"
 	"log"
+	"github.com/gohouse/gorose"
+	"github.com/cihub/seelog"
 )
 
 var SqlDB *sql.DB
@@ -15,4 +17,19 @@ func _init()  {
 	err2 := SqlDB.Ping()
 	if err1 != nil {log.Fatal(err1)}
 	if err2 != nil {log.Fatal(err2)}
+}
+func loadCon() *gorose.Connection {
+	var dbConfig = &gorose.DbConfigSingle{
+		Driver:          "mysql", // driver: mysql/sqlite/oracle/mssql/postgres
+		EnableQueryLog:  true,    // if enable sql logs
+		SetMaxOpenConns: 0,       // connection pool of max Open connections, default zero
+		SetMaxIdleConns: 0,       // connection pool of max sleep connections
+		Prefix:          "",      // prefix of table
+		Dsn:             "root:root@tcp(localhost:3306)/test?charset=utf8", // db dsn
+	}
+	var connection,err = gorose.Open(dbConfig)
+	if err != nil {
+		seelog.Info("mysql connection error=",err)
+	}
+	return connection
 }
